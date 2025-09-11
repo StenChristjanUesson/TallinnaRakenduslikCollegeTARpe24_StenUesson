ad5263c14a8347d1fa83db92f5e1e22a81b010ee
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-09-11 12:44:01 +0300

    Student Edit View added and create view Fixed

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -52,6 +52,53 @@ namespace TallinnaRakenduslikCollegeTARpe24_StenUesson.Controllers
             return View(student);
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,EnrollmentDate,Age,Gender")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -77,6 +124,11 @@ namespace TallinnaRakenduslikCollegeTARpe24_StenUesson.Controllers
             _context.Students.Remove(student);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.Id == id);
         }
     }
 }
